5c68f25646d9005e9eb110a266bce0ca6a63eb39
孙雨鹏 <1293291031@qq.com>
2019-07-07 17:42:06 +0800

    add: 获取邮箱内容

---
+++ src/main/java/com/cn/message/MessageApplication.java
@@ -0,0 +1,22 @@
+package com.cn.message;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author syp
+ * @package_name com.cn.message
+ * @date 2019-07-07 16:44
+ * @description
+ **/
+@SpringBootApplication
+
+public class MessageApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(MessageApplication.class, args);
+    }
+}
+
+
